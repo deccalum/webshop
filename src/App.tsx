4cc228ca7dcd70f679543bf964062dd18b38c6d3
Dennis <dcalmius@gmail.com>
2026-04-15 10:46:19 +0200

    refactor: restructure App component and add new layout components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,21 @@
+import Header from './components/Header'
+import Main from './components/Main'
+import Sidebar from './components/Sidebar'
+import Footer from './components/Footer'
+import ProductList from './components/Productlist'
+import Banner from './components/Banner'
+
+
 function App() {
   return (
     <>
+      <Header />
+      <Main>
+        <Sidebar />
+        <ProductList />
+      </Main>
+      <Banner />
+      <Footer />
     </>
   )
 }
